9c7a69ceccdf0a98bbc321b4ab0810798d239cba
Dominik Dubovec <dominik.dubovec.official@gmail.com>
2022-10-02 17:26:57 +0200

    Created abstract tree and abstract tree node

---
+++ Structures/Tree/AbstractTree.cs
@@ -0,0 +1,15 @@
+﻿namespace Structures.Tree
+{
+    public abstract class AbstractTree<TKey,TData> where TKey: IComparable<TKey>
+    {
+        protected abstract AbstractTreeNode<TKey, TData>? Root { get; set; }
+
+        protected abstract int Size { get; set; }
+
+        public abstract void Add(AbstractTreeNode<TKey, TData> newNode);
+
+        public abstract void Remove(AbstractTreeNode<TKey, TData> newNode);
+        
+        public abstract AbstractTreeNode<TKey, TData> Find(AbstractTreeNode<TKey, TData> newNode);
+    }
+}
