5ee50430f2c3b51b3453facf8f1a74d72de281c5
Dominik Dubovec <dominik.dubovec.official@gmail.com>
2022-10-03 20:34:17 +0200

    Added .NET MAUI project for the first semestral thesis

---
+++ SemestralnaPraca1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SemestralnaPraca1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
