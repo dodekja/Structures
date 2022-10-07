2633c67a19d2b092231d1c963fe8ba6be8cc0105
Dominik Dubovec <dominik.dubovec.official@gmail.com>
2022-10-07 17:19:41 +0200

    Added InOrder traversal tests, modified existing tests to use a fixture containing the tree instance

--- TestStructures/BinarySearchTreeUnitTest.cs
+++ TestStructures/BinarySearchTreeUnitTest.cs
@@ -1,52 +1,119 @@
-using System.Diagnostics;
 using Structures.Tree;
 
 namespace TestStructures
 {
     public class BinarySearchTreeUnitTest : IDisposable
     {
-        BinarySearchTree<int, int>? tree;
+        public BinarySearchTree<int, int>? Tree { get; private set; }
 
         public BinarySearchTreeUnitTest()
         {
-            tree = new BinarySearchTree<int, int>();
+            Tree = new BinarySearchTree<int, int>();
         }
 
         [Fact]
-        public void TestInsertSingleItem()
+        public void InsertSingleItem()
         {
             //Arrange
             var item = (1, 1);
             int treeSize = 0;
 
             //Act
-            tree?.Add(item.Item1,item.Item2);
-            treeSize = tree.GetSize();
+            Tree?.Add(item.Item1,item.Item2);
+            treeSize = Tree.GetSize();
 
             //Assert
-            Xunit.Assert.Same(item.Item2,tree.Find(1));
+            Assert.True(treeSize == 1, $"Tree size is {treeSize}, should be 1.");
         }
 
         [Fact]
-        public void TestRemoveSingleItem()
+        public void RemoveSingleItem()
         {
             //Arrange
             var item = (1, 1);
-            tree?.Add(item.Item1, item.Item2);
-            int treeSize = tree.GetSize();
+            Tree?.Add(item.Item1, item.Item2);
+            int treeSize = Tree.GetSize();
 
 
             //Act
-            tree.Remove(item.Item1);
-            treeSize = tree.GetSize();
+            Tree.Remove(item.Item1);
+            treeSize = Tree.GetSize();
 
             //Assert
-            Xunit.Assert.True(treeSize == 0);
+            Assert.True(treeSize == 0);
         }
+
+        [Fact]
+        public void InOrderTraversalEmpty()
+        {
+            //Arrange
+            List<Tuple<int, int>>? list;
+
+            //Act
+            list = Tree?.InOrder();
+
+            //Assert
+            Assert.True(list is { Count: 0 });
+        }
+
+        [Fact]
+        public void InOrderTraversalSingleItem()
+        {
+            //Arrange
+            List<Tuple<int, int>>? list = null;
+            Tuple<int, int> expectedItem = new Tuple<int, int>(1,1);
+            Tree?.Add(expectedItem.Item1, expectedItem.Item2);
+
+            //Act
+            list = Tree?.InOrder();
+
+            //Assert
+            Assert.Contains(expectedItem, list);
+        }
+
+        [Theory]
+        [MemberData(nameof(InOrderTraversalMultipleItemsData))]
+        public void InOrderTraversalMultipleItems(List<Tuple<int, int>> itemsToInsert, List<Tuple<int, int>> expectedList)
+        {
+            //Arrange
+            foreach (Tuple<int, int> tuple in itemsToInsert)
+            {
+                Tree?.Add(tuple.Item1,tuple.Item2);
+            }
+
+            List<Tuple<int, int>>? actualList = null;
+
+            //Act
+            actualList = Tree?.InOrder();
+
+            //Assert
+            Assert.Equal(expectedList, actualList);
+        }
+
+        public static IEnumerable<object[]> InOrderTraversalMultipleItemsData =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new List<Tuple<int,int>> {new(5,5), new(10,10), new(11,11), new(20,20), new(30,30), new(40,40), new(45,45)},
+                    new List<Tuple<int,int>> {new(5,5), new(10,10), new(11,11), new(20,20), new(30,30), new(40,40), new(45,45)}
+                },
+                new object[] {new List<Tuple<int,int>> {new(5,5), new(15,15), new(18,18), new(20,20), new(25,25), new(30,30), new(35,35),
+                    new(40, 40), new(45, 45), new(50, 50), new(60, 60) },
+                    new List<Tuple<int,int>> {new(5,5), new(15,15), new(18,18), new(20,20), new(25,25), new(30,30), new(35,35),
+                        new(40, 40), new(45, 45), new(50, 50), new(60, 60) }
+                },
+                new object[] {new List<Tuple<int,int>> {new(30,30), new(20,20), new(40,40), new(15,15), new(25,25), new(35,35), new(50,50),
+                        new(5, 5), new(18, 18), new(45, 45), new(60, 60) },
+                    new List<Tuple<int,int>> {new(5,5), new(15,15), new(18,18), new(20,20), new(25,25), new(30,30), new(35,35),
+                        new(40, 40), new(45, 45), new(50, 50), new(60, 60) }
+                },
+
+            };
 
         public void Dispose()
         {
-            tree = null;
+            Tree = null;
         }
     }
 }
